39e5c330b3f505e484eb663161ccf2a6170d77d8
infinityPV <31202524+infinityPV@users.noreply.github.com>
2018-03-06 08:19:46 +0100

    Update and rename infinityPV_INA233_Read_voltage.ino to infinityPV_INA233_Read_voltages.ino

--- example/infinityPV_INA233_Read_voltages.ino
+++ example/infinityPV_INA233_Read_voltages.ino
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
 // 
-// infinityPV_INA233_Read_voltage.ino
+// infinityPV_INA233_Read_voltages.ino
 //
 // This is an example for using the LiBAT5V2A Energy Management shield    
 // from infinityPV with Arduino.                                    
@@ -19,6 +19,10 @@
 //   -getBusVoltage_V()
 // The function returns a float containing the measured voltage in V.  
 //
+// We have also implemented a another function to read easily the 
+// value of voltage read on the shunt resistor, in case this value is needed:
+//    -getShuntVoltage_mV()
+// The function returns a float containing the measured shunt voltage in mV. 
 //
 // (*) The example uses the infinityPV_INA233 library, which licensed 
 // under BSD license. 
@@ -36,7 +40,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Hej!");
   
-  Serial.println("Measuring voltage at IC1 and IC2 INA233 at LiBAT5V2A Energy Management Shield");
+  Serial.println("Measuring voltages at IC1 and IC2 INA233 at LiBAT5V2A Energy Management Shield");
   IC1.begin();
   IC2.begin();
 }
@@ -44,13 +48,19 @@ void setup() {
 void loop() {
   float voltage_IC1=0;
   float voltage_IC2=0;
+  float shunt_voltage_IC1=0;
+  float shunt_voltage_IC2=0;
 
   voltage_IC1=IC1.getBusVoltage_V();  //getting bus voltage (V) at INA233 IC1
   voltage_IC2=IC2.getBusVoltage_V();  //getting bus voltage (V) at INA233 IC2
-
+  shunt_voltage_IC1=IC1.getShuntVoltage_mV();  //getting shunt voltage (mV) at INA233 IC1
+  shunt_voltage_IC2=IC2.getShuntVoltage_mV();  //getting shunt voltage (mV) at INA233 IC2
+                   
   
   Serial.print(" Battery voltage:   "); Serial.print(voltage_IC2);Serial.println(" V");
   Serial.print(" Output voltage:   "); Serial.print(voltage_IC1);Serial.println(" V");
+  Serial.print(" Shunt voltage at IC1:   "); Serial.print(shunt_voltage_IC1);Serial.println(" mV");
+  Serial.print(" Shunt voltage at IC2:   "); Serial.print(shunt_voltage_IC2);Serial.println(" mV");
   
   delay(2000);
 
