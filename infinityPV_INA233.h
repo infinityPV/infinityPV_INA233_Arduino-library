89ab4ad23cfa3dfec5c214e81abebaf5151d064e
infinityPV <31202524+infinityPV@users.noreply.github.com>
2018-03-07 09:10:04 +0100

    Update infinityPV_INA233.h

--- infinityPV_INA233.h
+++ infinityPV_INA233.h
@@ -93,52 +93,6 @@
 
 /*=========================================================================*//*=========================================================================
 
-/*=========================================================================
-    CONFIG REGISTER (R/W)
-    -----------------------------------------------------------------------*/
-    #define INA233_REG_CONFIG                      (0x00)
-    /*---------------------------------------------------------------------*/
-    #define INA233_CONFIG_RESET                    (0x8000)  // Reset Bit
-
-    #define INA233_CONFIG_BVOLTAGERANGE_MASK       (0x2000)  // Bus Voltage Range Mask
-    #define INA233_CONFIG_BVOLTAGERANGE_16V        (0x0000)  // 0-16V Range
-    #define INA233_CONFIG_BVOLTAGERANGE_32V        (0x2000)  // 0-32V Range
-
-    #define INA233_CONFIG_GAIN_MASK                (0x1800)  // Gain Mask
-    #define INA233_CONFIG_GAIN_1_40MV              (0x0000)  // Gain 1, 40mV Range
-    #define INA233_CONFIG_GAIN_2_80MV              (0x0800)  // Gain 2, 80mV Range
-    #define INA233_CONFIG_GAIN_4_160MV             (0x1000)  // Gain 4, 160mV Range
-    #define INA233_CONFIG_GAIN_8_320MV             (0x1800)  // Gain 8, 320mV Range
-
-    #define INA233_CONFIG_BADCRES_MASK             (0x0780)  // Bus ADC Resolution Mask
-    #define INA233_CONFIG_BADCRES_9BIT             (0x0080)  // 9-bit bus res = 0..511
-    #define INA233_CONFIG_BADCRES_10BIT            (0x0100)  // 10-bit bus res = 0..1023
-    #define INA233_CONFIG_BADCRES_11BIT            (0x0200)  // 11-bit bus res = 0..2047
-    #define INA233_CONFIG_BADCRES_12BIT            (0x0400)  // 12-bit bus res = 0..4097
-
-    #define INA233_CONFIG_SADCRES_MASK             (0x0078)  // Shunt ADC Resolution and Averaging Mask
-    #define INA233_CONFIG_SADCRES_9BIT_1S_84US     (0x0000)  // 1 x 9-bit shunt sample
-    #define INA233_CONFIG_SADCRES_10BIT_1S_148US   (0x0008)  // 1 x 10-bit shunt sample
-    #define INA233_CONFIG_SADCRES_11BIT_1S_276US   (0x0010)  // 1 x 11-bit shunt sample
-    #define INA233_CONFIG_SADCRES_12BIT_1S_532US   (0x0018)  // 1 x 12-bit shunt sample
-    #define INA233_CONFIG_SADCRES_12BIT_2S_1060US  (0x0048)	 // 2 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_4S_2130US  (0x0050)  // 4 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_8S_4260US  (0x0058)  // 8 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_16S_8510US (0x0060)  // 16 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_32S_17MS   (0x0068)  // 32 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_64S_34MS   (0x0070)  // 64 x 12-bit shunt samples averaged together
-    #define INA233_CONFIG_SADCRES_12BIT_128S_69MS  (0x0078)  // 128 x 12-bit shunt samples averaged together
-
-    #define INA233_CONFIG_MODE_MASK                (0x0007)  // Operating Mode Mask
-    #define INA233_CONFIG_MODE_POWERDOWN           (0x0000)
-    #define INA233_CONFIG_MODE_SVOLT_TRIGGERED     (0x0001)
-    #define INA233_CONFIG_MODE_BVOLT_TRIGGERED     (0x0002)
-    #define INA233_CONFIG_MODE_SANDBVOLT_TRIGGERED (0x0003)
-    #define INA233_CONFIG_MODE_ADCOFF              (0x0004)
-    #define INA233_CONFIG_MODE_SVOLT_CONTINUOUS    (0x0005)
-    #define INA233_CONFIG_MODE_BVOLT_CONTINUOUS    (0x0006)
-    #define INA233_CONFIG_MODE_SANDBVOLT_CONTINUOUS (0x0007)
-/*=========================================================================*/
 
 /*=========================================================================
     SHUNT VOLTAGE TELEMETRY & WARNING COEFFICIENTS
@@ -157,22 +111,12 @@
 /*=========================================================================*/
 
 /*=========================================================================
-    POWER REGISTER (R)
+    CURRENT & POWER CONSTANT TELEMETRY & WARNING COEFFICIENTS
     -----------------------------------------------------------------------*/
-    #define INA233_REG_POWER                       (0x03)
+    #define b_c                (0)
+    #define b_p                (0)
 /*=========================================================================*/
 
-/*=========================================================================
-    CURRENT REGISTER (R)
-    -----------------------------------------------------------------------*/
-    #define INA233_REG_CURRENT                     (0x04)
-/*=========================================================================*/
-
-/*=========================================================================
-    CALIBRATION REGISTER (R/W)
-    -----------------------------------------------------------------------*/
-    #define INA233_REG_CALIBRATION                 (0x05)
-/*=========================================================================*/
 
 class INA233{
  public:
@@ -181,9 +125,13 @@ class INA233{
   float getBusVoltage_V(void);
   float getShuntVoltage_mV(void);
   float getCurrent_mA(void);
+  float getPower_mW(void);
+  float getAv_Power_mW(void);
   int16_t getBusVoltage_raw(void);
   int16_t getShuntVoltage_raw(void);
   int16_t getCurrent_raw(void);
+  int16_t getPower_raw(void);
+  void getEnergy_raw(uint16_t* accumulator, uint8_t* roll_over, uint32_t* sample_count);
   uint16_t setCalibration(float r_shunt,float i_max,float *Current_LSB,float *Power_LSB, int16_t *m_c,int8_t *R_c, int16_t *m_p, int8_t *R_p,  uint8_t *ERROR);
   void wireReadWord(uint8_t reg, uint16_t *value);
   void wireReadByte(uint8_t reg, uint8_t *value);
@@ -194,10 +142,12 @@ class INA233{
  private:
   uint8_t ina233_i2caddr;
   uint32_t ina233_calValue;
-  // The following multipliers are used to convert raw current and power
+  // The following coefficients are used to convert raw current and power
   // values to mA and mW, taking into account the current config settings
-  uint32_t ina233_currentDivider_mA;
-  uint32_t ina233_powerDivider_mW;
+  int16_t m_c;
+  int8_t R_c;
+  int16_t m_p;
+  int8_t R_p;
   void wireWriteRegister(uint8_t reg, uint16_t value);
   void wireReadRegister(uint8_t reg, uint16_t *value);
 
